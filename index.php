e1f45f61e8706ae9beaa4fe3a44ba2db5f9c6180
Alkane Mallari <root.kiyen@gmail.com>
2016-05-16 23:06:31 +0800

    index.php reuploaded

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Tarlac First Baptist Church School</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Orgen Corpuz">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!-- Google materialize icons -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="navbar-fixed">
+			<nav class="homenav z-depth-1">
+			    <div class="nav-wrapper container navContent">
+			      <a href="#" class="brand-logo"><img src="img/TFBCS_LOGO.png" alt="" style="height: 100px; width: 100px; margin-top: 5px;"></a>
+			      <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
+			      	<ul id="nav-mobile" class="right hide-on-med-and-down">
+				        <li class="active"><a href="index.html">HOME</a></li>
+				        <li><a href="about.html">ABOUT</a></li>
+				        <li><a href="admission.html">ADMISSION</a></li>
+				        <li><a href="academics.html">ACADEMICS</a></li>
+				        <li><a href="ministry.html">MINISTRY</a></li>
+				        <li><a href="alimnihomecoming.html">ALUMNI</a></li>
+				        <li><a href="contact.html">CONTACT</a></li>
+				        <li><a class="dropdown-button" href="#!" data-activates="portal" data-beloworigin="true">PORTAL<i class="material-icons right">arrow_drop_down</i></a></li>
+				        <li><a href="#"><i class="material-icons">search</i></a></li>
+			      	</ul>
+			      	<ul class="side-nav" id="mobile-nav">
+				        <li><a href="index.html">HOME</a></li>
+				        <li><a href="about.html">ABOUT</a></li>
+				        <li><a href="admission.html">ADMISSION</a></li>
+				        <li><a href="academics.html">ACADEMICS</a></li>
+				        <li><a href="ministry.html">MINISTRY</a></li>
+				        <li><a href="alimnihomecoming.html">ALUMNI</a></li>
+				        <li><a href="facultyportal.html">FACULTY PORTAL</a></li>
+				        <li><a href="studentportal.html">STUDENT PORTAL</a></li>
+				        <li><a href="contact.html">CONTACT</a></li>
+				        <li><a href="#"><i class="material-icons">search</i></a></li>
+			      	</ul>
+			      	<ul id="portal" class="dropdown-content">
+						<li><a href="#!">Faculty</a></li>
+						<li><a href="#!">Student</a></li>
+						<li class="divider"></li>
+						<li><a href="#!">Principal</a></li>
+					</ul>
+			    </div>
+		  	</nav>
+	  	</div>
+	</header>
+	<main>
+		<div class="slider z-depth-1">
+		    <ul class="slides">
+		      <li>
+		        <img src="img/1.jpg" class="img-responsive"> <!-- random image -->
+		      </li>
+		      <li>
+		        <img src="img/2.jpg" class="img-responsive"> <!-- random image -->
+		        <div class="caption left-align">
+		          <h3>Left Aligned Caption</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		      <li>
+		        <img src="img/3.jpg" class="img-responsive"> <!-- random image -->
+		        <div class="caption right-align">
+		          <h3>Right Aligned Caption</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		      <li>
+		        <img src="img/4.jpg" class="img-responsive"> <!-- random image -->
+		        <div class="caption center-align">
+		          <h3>This is our big Tagline!</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		    </ul>
+		</div>
+
+		<section class="container aboutTfbcs">
+			<h3>Tarlac First Baptist Church School</h3>
+		</section>
+
+	</main>
+	<footer class="page-footer">
+		<div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Tarlac First Baptist Church School</h5>
+                <p class="grey-text text-lighten-4">"We have a destiny with a distinction to make a difference"</p>
+                <h4 class="grey-text text-lighten-4">DEI GRATIA!</h4>
+                <div class="address">
+                	<p class="grey-text text-lighten-4">
+                		<i class="material-icons">location_on</i>&nbsp;&nbsp;
+                		Romulo Boulevard, San Vicente
+                		Tarlac City, Tarlac Philippines
+                		<br><br>
+                		<i class="material-icons">local_phone</i>&nbsp;&nbsp;982-0996 (Main Building)<br> 
+                		<i class="material-icons">local_phone</i>&nbsp;&nbsp;982-3825 (Annex-Elementary)<br> 
+                		<i class="material-icons">local_phone</i>&nbsp;&nbsp;628-2868 (Annex-High School)
+                		<br><br> 
+                		<i class="material-icons">mail_outline</i>&nbsp;&nbsp;tfbcschool_inc@yahoo.com
+                	</p>
+                </div>
+              </div>
+              <div class="col l4 offset-l2 s12">
+		            <div class="socialmedia">
+		            	<h5 class="white-text">Social Media - Follow Us</h5>
+		              	<ul>
+		                  <li style="display: inline-block;"><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+		                  <li style="display: inline-block;"><a class="grey-text text-lighten-3" href="#!">-&nbsp;&nbsp;Twitter&nbsp;&nbsp;-</a></li>
+		                  <li style="display: inline-block;"><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+	                	</ul>
+		            </div>
+		            <div class="links">
+		            	<h5 class="white-text">Helpful Links</h5>
+		                <ul>
+		                  <li><a class="grey-text text-lighten-3" href="#">School Announcements</a></li>
+		                  <li><a class="grey-text text-lighten-3" href="#">Church Activities</a></li>
+		                  <li><a class="grey-text text-lighten-3" href="#">Events</a></li>
+		                  <li><a class="grey-text text-lighten-3" href="#">Privacy Policy</a></li>
+		                </ul>
+		            </div>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+           		<span>&copy; <?php echo date("Y") ?> Tarlac First Baptist Church School</span>
+            	<a class="grey-text text-lighten-4 right gototop" href="#!">Go to top&nbsp;<i class="material-icons">arrow_drop_up</i></a>
+            </div>
+       	</div>
+		
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-2.2.3.min.js">\x3C/script>')</script> <!-- load this if cdn fails -->
+	<script src="js/pace.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
